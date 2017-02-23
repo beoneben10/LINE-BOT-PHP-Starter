84fdb3a0061f62dd0061b29bf65229ebc6d399ba
beoneben10 <bestthan01@gmail.com>
2017-02-23 15:34:15 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,8 +1,8 @@
 <?php
 include 'msg.php';
 $access_token = 'SmC479aBsZtxrVoqsqEW1KDMT6c/bP6woDbRg2BD56+k/NM86O4XKvG68KVShh4mImg9IQJbE+QaVdGlSUSAbhzuxYN60cRrWdrqa6eyTAf2aOZO2IxluB9A4tAFYjepYUVSX+R1OKfrl9JX2zlc3QdB04t89/1O/w1cDnyilFU=';
-$mm = new msg();
 // Get POST body content
+$mm = new msg();
 $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
@@ -13,9 +13,12 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
+			$this->con($event['replyToken'],$event['source']['userId'],$event['message']['id'],$event['message']['text']);
 			//$text = $event['message']['text'];
-			$mm->showMSG($event['replyToken'],$event['source']['userId'],$event['message']['text']);
+			else {$mm->showMSG($event['replyToken'],$event['source']['userId'],$event['message']['text']);
+			     }
 		}
 	}
 }
-echo "OK";
+
+?>
